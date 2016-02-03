3c9234b900376622f54f46b2237a80dc8e569922
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2016-02-03 14:21:04 +0900

    implement print output.

--- examples/legacy/print_output.py
+++ examples/legacy/print_output.py
@@ -3,10 +3,9 @@
 
 import sys
 from pystargazer import *
-import time
 
 serial_device = sys.argv[1]
 sg = StarGazer(serial_device)
 while True:
-    output = sg.read_output()
-    print(output)
+    line = sg.read_raw_output()
+    print(line)
--- pystargazer/stargazer.py
+++ pystargazer/stargazer.py
@@ -1,13 +1,131 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from . import serial_manager
 from .utils import *
 import re
+from threading import Thread, Event
+import serial
+import time
+import re
 
+DEBUG = False
+
+STX = '~'  # start of text data
+ETX = '`'  # end of text data
+
+READ_COMMAND_PREFIX = '@'
+WRITE_COMMAND_PREFIX = '#'
+RETURN_VALUE_PREFIX = '$'
+ACK_PREFIX = '!'
+MESSAGE_PREFIX = '*'
+VALUE_SEPARATER = '|'
+
+# Message example:
+# ~^124610|-3.70|+22.96|+98.73|216.09`
+# ~^224610|-65.20|+22.18|+77.31|209.55|24594|-66.66|+22.79|-94.34|210.63`
+
+DATA_REGEX = r'([0-9]+)\|([\+\-][0-9]+\.?[0-9]+)\|([\+\-][0-9]+\.?[0-9]+)\|([\+\-][0-9]+\.?[0-9]+)\|([0-9]+\.?[0-9]+)'
+ONE_DATA_REGEX = STX + r'\^1' + DATA_REGEX + ETX
+TWO_DATA_REGEX = STX + r'\^2' + DATA_REGEX + VALUE_SEPARATER + ETX
+
+MAP_MODE_DATA_REGEX = r'~\^I([0-9]+)\|([\+\-][0-9]+\.?[0-9]+)\|([\+\-][0-9]+\.?[0-9]+)\|([\+\-][0-9]+\.?[0-9]+)\|([0-9]+\.?[0-9]+)`'
+DEAD_ZONE_MESSAGE_REGEX = r'~\*DeadZone`'
+MAP_ID_MESSAGE_REGEX = r'~\*MAPID\|([0-9]+)`'
+MAP_DATA_SAVE_REGEX = r'~\!MapDataSave`'
+PARAMETER_UPDATE_REGEX = r'~\!ParameterUpdate`'
+
+_SEND_CHARACTER_INTERVAL = 0.01
+_CHECK_ACK_WAIT_TIME = 1.0
+_READ_RESPONSE_WAIT_TIME = 1.0
+_PARAMETER_UPDATE_WAIT_TIME = 50.0
 
 class StarGazer(object):
     """docstring for StarGazer"""
+
+    def __init__(self, serial_device_name, map_coordinate_file=None, multi_id=True):
+        if not multi_id:
+            print("Single ID version is not implemented now. use legacy version.")
+        super(StarGazer, self).__init__()
+        self.__is_multi_id = multi_id
+        self.__latest_output = None
+        self.serial = serial.Serial(serial_device_name,
+                                    baudrate=115200,
+                                    bytesize=serial.EIGHTBITS,
+                                    parity=serial.PARITY_NONE,
+                                    stopbits=serial.STOPBITS_ONE,
+                                    timeout=0)
+        self.update_event = Event()
+        self.serial_io_thread = Thread(target=self.__run_serial_io)
+        self.serial_io_thread.start()
+
+
+    # blocking method
+    def read_status(self):
+        return None
+
+
+    def get_latest_status(self):
+        return this.current_status
+
+
+    # blocking method
+    def read_output(self):
+        self.__wait_update()
+        return self.get_latest_output()
+
+
+    def get_latest_output(self):
+        return self.__latest_output
+
+
+    def __run_serial_io(self):
+        while True:
+            self.__update()
+
+
+    def __update(self):
+        self.update_event.clear()
+        line = self.__read_line(timeout=None)
+        if line is not None:
+            self.__latest_output = line;
+        self.update_event.set()
+
+
+    def __wait_update(self):
+        self.update_event.wait()
+
+
+    def __read_line(self, timeout=None):
+        byte_data = b''
+        etx_byte = bytes(ETX, 'ascii')
+        start_time = time.time()
+        while True:
+            c = self.serial.read(1)
+            byte_data += c
+            if c == etx_byte:
+                break
+            if (timeout != None) and (time.time()-start_time > timeout):
+                break
+        try:
+            line = byte_data.decode()
+            self.__print_debug("recv: "+line)
+            return line
+        except Exception as e:
+            self.__print_debug(e)
+            return None
+
+
+    def __print_debug(self, msg):
+        if DEBUG:
+            print('\033[94m [DEBUG] \033[0m', msg)
+
+
+
+
+    #====================
+    # Legacy code
+    #====================
+class StarGazerLegacy(object):
     def __init__(self, serial_device_name):
         super(StarGazer, self).__init__()
         self.sm = serial_manager.SerialManager(serial_device_name)
