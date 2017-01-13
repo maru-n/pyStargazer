fixed bug on calculation of z-axis.

pystargazer/stargazer_data.py
@@ -43,7 +43,7 @@ class StargazerData(LocationData):
             marker_map = {}
         try:
             for i in marker_map:
-                if len(marker_map[i]) != 4:
+                if len(marker_map[i]) != 3:
                     raise StargazerException("Invalid marker_map data on {} with {}".format(i, marker_map[i]))
         except Exception as e:
             raise StargazerException("Invalid marker_map data.")
@@ -60,15 +60,15 @@ class StargazerData(LocationData):
 
         if two_data_match:
             data = two_data_match.groups()
-            ld1 = LocationData(int(data[0]), *[float(v)*0.01 for v in data[2:5]], float(data[1]) * np.pi / 180)
-            ld2 = LocationData(int(data[5]), *[float(v)*0.01 for v in data[7:10]], float(data[6]) * np.pi / 180)
+            ld1 = LocationData(int(data[0]), float(data[2])*0.01, float(data[3])*0.01, -float(data[4])*0.01, float(data[1]) * np.pi / 180)
+            ld2 = LocationData(int(data[5]), float(data[7])*0.01, float(data[8])*0.01, -float(data[9])*0.01, float(data[6]) * np.pi / 180)
             self.local_location.append(ld1)
             self.local_location.append(ld2)
             self.observed_markers.append(ld1.marker_id)
             self.observed_markers.append(ld2.marker_id)
         elif one_data_match:
             data = one_data_match.groups()
-            ld = LocationData(int(data[0]), *[float(v)*0.01 for v in data[2:5]], float(data[1]) * np.pi / 180)
+            ld = LocationData(int(data[0]), float(data[2])*0.01, float(data[3])*0.01, -float(data[4])*0.01, float(data[1]) * np.pi / 180)
             self.local_location.append(ld)
             self.observed_markers.append(ld.marker_id)
         elif re.match(MESSAGE_UTIL.DEAD_ZONE_MESSAGE_REGEX, output_str):
@@ -83,11 +83,11 @@ class StargazerData(LocationData):
                 if self.angle is None:
                     self.angle = self.x = self.y = self.z = 0.0
                 self.marker_id.append(mid)
-                abs_location = np.array(marker_map[mid]) + np.array(loc)
+                abs_location = np.array(marker_map[mid]) + np.array(loc)[0:3]
                 self.x += abs_location[0]
                 self.y += abs_location[1]
                 self.z += abs_location[2]
-                self.angle += abs_location[3]
+                self.angle += loc.angle
                 dnum += 1
 
         if dnum != 0:

pystargazer/utils.py
@@ -6,15 +6,15 @@ import warnings
 import numpy as np
 
 
-# usecols=(id, x, y, z, angle)
+# usecols=(id, x, y, z)
 # default is none that mean (0,1,2,3,4)
 def load_marker_map(fname, comments='#', delimiter=None, skiprows=0, usecols=None):
     raw_data = np.loadtxt(fname, comments=comments, delimiter=delimiter, skiprows=skiprows, usecols=usecols)
     marker_map = {}
     for l in raw_data:
         marker_id = int(l[0])
-        x, y, z, angle = l[1:5]
-        marker_map[marker_id] = [x, y, z, angle]
+        x, y, z = l[1:4]
+        marker_map[marker_id] = [x, y, z]
     return marker_map
 
 

tests/test_stargazer_data.py
@@ -9,14 +9,14 @@ class TestStargazerData(unittest.TestCase):
         self.output_str_multiid_2 = "~^224706|-96.28|-48.90|-21.13|220.50|24594|-94.27|+101.58|-17.80|230.71`"
         self.output_str_deadzone = "~*DeadZone`"
         self.marker_map = {
-            #marker_id: [x,y,z,angle]
-            24706: [1.5, -1.5, 0.0, 0.0],
-            24594: [0.0, -1.5, 0.0, 0.0]
+            #marker_id: [x,y,z]
+            24706: [1.5, -1.5, 0.0],
+            24594: [0.0, -1.5, 0.0]
         }
         self.data_multiid_1 = StargazerData(self.output_str_multiid_1, marker_map=self.marker_map)
         self.data_multiid_2 = StargazerData(self.output_str_multiid_2, marker_map=self.marker_map)
         self.data_without_map = StargazerData(self.output_str_multiid_2)
-        self.data_no_match_map = StargazerData(self.output_str_multiid_2, marker_map={1:[1,1,1,1],2:[2,2,2,2]})
+        self.data_no_match_map = StargazerData(self.output_str_multiid_2, marker_map={1:[1,1,1],2:[2,2,2]})
         self.data_deadzone = StargazerData(self.output_str_deadzone)
 
     def tearDown(self):
@@ -63,7 +63,7 @@ class TestStargazerData(unittest.TestCase):
         self.assertEqual(d[0].marker_id, 24706)
         self.assertAlmostEqual(d[0].x, -48.13*0.01)
         self.assertAlmostEqual(d[0].y, -21.35*0.01)
-        self.assertAlmostEqual(d[0].z, 221.64*0.01)
+        self.assertAlmostEqual(d[0].z, -221.64*0.01)
         self.assertAlmostEqual(d[0].angle, -96.61 * np.pi / 180)
 
     def test_marker_id_2(self):
@@ -78,12 +78,12 @@ class TestStargazerData(unittest.TestCase):
         self.assertEqual(d[0].marker_id, 24706)
         self.assertAlmostEqual(d[0].x, -48.90*0.01)
         self.assertAlmostEqual(d[0].y, -21.13*0.01)
-        self.assertAlmostEqual(d[0].z, 220.50*0.01)
+        self.assertAlmostEqual(d[0].z, -220.50*0.01)
         self.assertAlmostEqual(d[0].angle, -96.28 * np.pi / 180)
         self.assertEqual(d[1].marker_id, 24594)
         self.assertAlmostEqual(d[1].x, 101.58*0.01)
         self.assertAlmostEqual(d[1].y, -17.80*0.01)
-        self.assertAlmostEqual(d[1].z, 230.71*0.01)
+        self.assertAlmostEqual(d[1].z, -230.71*0.01)
         self.assertAlmostEqual(d[1].angle, -94.27 * np.pi / 180)
 
     def test_global_location_1(self):
@@ -92,7 +92,7 @@ class TestStargazerData(unittest.TestCase):
         self.assertEqual(d.marker_id[0], 24706)
         self.assertAlmostEqual(d.x, -48.13*0.01 + 1.5)
         self.assertAlmostEqual(d.y, -21.35*0.01 - 1.5)
-        self.assertAlmostEqual(d.z, 221.64*0.01 + 0.0)
+        self.assertAlmostEqual(d.z, -221.64*0.01 + 0.0)
         self.assertAlmostEqual(d.angle, -96.61 * np.pi / 180 + 0.0)
 
     def test_global_location_2(self):
@@ -102,7 +102,7 @@ class TestStargazerData(unittest.TestCase):
         self.assertEqual(d.marker_id[1], 24594)
         self.assertAlmostEqual(d.x, (-48.90*0.01+1.5 + 101.58*0.01+0.0)/2)
         self.assertAlmostEqual(d.y, (-21.13*0.01-1.5 -  17.80*0.01-1.5)/2)
-        self.assertAlmostEqual(d.z, (220.50*0.01+0.0 + 230.71*0.01+0.0)/2)
+        self.assertAlmostEqual(d.z, -(220.50*0.01+0.0 + 230.71*0.01+0.0)/2)
         self.assertAlmostEqual(d.angle, (-96.28 - 94.27) * np.pi / 180 / 2)
 
 